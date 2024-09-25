fd507b805a030d01c6d09fc3048306909dc6d45a
Varun Naik <Varunnaik2001@gmail.com>
2024-09-25 14:45:09 -0500

    added connection to mongodb

--- chromeapi/server.js
+++ chromeapi/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config({path: "./config.env"});
 const app = require("./app");
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace("<db_password>", process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
