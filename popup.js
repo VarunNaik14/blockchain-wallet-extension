eacef10f0600e5031e23fa6880075830123959cd
Varun Naik <Varunnaik2001@gmail.com>
2024-12-07 18:35:48 -0600

    moved render component to its own function

--- popup.js
+++ popup.js
@@ -220,6 +220,7 @@ function getSelectedNetwork(e){
     }   
 
     checkBalance();
+    render();
     console.log(providerURL);
 };
 
@@ -292,8 +293,6 @@ function signUp(){
                 private_key: wallet.privateKey,
                 mnemonic: wallet.mnemonic.phrase,
             };
-
-            console.log("New Wallet Created w/ Signup: ", userWallet);
 
             const jsonObj = JSON.stringify(userWallet);
             localStorage.setItem("userWallet", jsonObj);
@@ -465,20 +464,10 @@ function addAccount(){
 
 };
 
-function myFunction(){
-    const str = localStorage.getItem("userWallet");
-    const parsedObj = JSON.parse(str);
+function render(){
 
-    if(parsedObj?.address){
-        document.getElementById("LoginUser").style.display = "none";
-        document.getElementById("home").style.display = "block";
-
-        privateKey = parsedObj.private_key;
-        address = parsedObj.address;
-
-        checkBalance(parsedObj.address);
-    }
-
+    console.log("render called!");
+    
     const tokenRender = document.querySelector(".assets");
     const accountRender = document.querySelector(".accountList");
 
@@ -541,7 +530,86 @@ function myFunction(){
 
         }).catch(error => console.log(error))
 
-        console.log(privateKey);
+};
+
+function myFunction(){
+    const str = localStorage.getItem("userWallet");
+    const parsedObj = JSON.parse(str);
+
+    if(parsedObj?.address){
+        document.getElementById("LoginUser").style.display = "none";
+        document.getElementById("home").style.display = "block";
+
+        privateKey = parsedObj.private_key;
+        address = parsedObj.address;
+
+        checkBalance(parsedObj.address);
+    }
+
+    render();
+
+    // const tokenRender = document.querySelector(".assets");
+    // const accountRender = document.querySelector(".accountList");
+
+    // const url = "http://localhost:3000/api/v1/tokens/alltoken";
+    // fetch(url).then((response) => response.json()).then((data) =>{
+    //     let elements = "";
+
+    //     data.data.tokens.map((token) =>{
+    //         if(token.provider_url === providerURL){
+    //             (
+    //                 elements += `
+    //                 <div class ="assets_item">
+    //                     <img class ="assets_item_img"
+    //                     src="./assets/logo.png"
+    //                     alt =""
+    //                     />
+
+    //                     <span>${checkTokenBalance(token.address)} </span>
+    //                     <span>${token.address.slice(0,15)}...</span>
+    //                     <span>${token.symbol}</span>
+        
+    //                 </div>
+    //                 `
+    //             )
+    //         }
+    //     });
+
+    //     tokenRender.innerHTML = elements;
+
+    // })
+    // .catch(error => console.log(error));
+
+    // fetch("http://localhost:3000/api/v1/account/allaccount").then((response) => 
+    //     response.json())
+    //     .then((data) => {
+    //         let accounts ="";
+
+    //         data.data.accounts.map((account, i) => {
+    //             accounts += `
+    //             <div class="lists" data-address="${account.address}" data-privatekey="${account.privateKey}" >
+    //             <p> ${i + 1} </p>
+    //             <p class="accountValue">
+    //              ${account.address.slice(0,25)}...
+    //              </p>
+    //             </div>
+    //             `
+    //         })
+
+    //         accountRender.innerHTML = accounts
+
+    //         document.querySelectorAll('.lists').forEach(list => {
+
+    //             list.addEventListener('click', (event) => {
+    //                 const accountAddress = event.currentTarget.getAttribute('data-address');
+    //                 const accountPrivateKey = event.currentTarget.getAttribute('data-privatekey');
+    //                 changeAccount(accountAddress,accountPrivateKey);}
+    //             );
+
+    //         })
+
+    //     }).catch(error => console.log(error))
+
 };
 
 function copyAddress(){
